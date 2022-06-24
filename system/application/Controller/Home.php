0a707deb800be83749a83c4cf96ecdd7aa22fc5f
Gabriel Scatolin <gscatolin@outlook.com>
2022-06-24 13:11:02 -0300

    fetch fix

--- system/application/Controller/Home.php
+++ system/application/Controller/Home.php
@@ -7,8 +7,18 @@ use Inphinit\Viewing\View;
 class Home
 {
     const NOT_ALLOWED_EXTENSIONS = [
-        "php", "xphp", "php5"
+        "php", "xphp", "php5", "sh"
     ];
+
+    function generateUpToDateMimeArray($url)
+    {
+        $s = array();
+        foreach (explode("\n", file_get_contents($url)) as $x)
+            if (isset($x[0]) && $x[0] !== '#' && preg_match_all('#([^\s]+)#', $x, $out) && isset($out[1]) && ($c = count($out[1])) > 1)
+                for ($i = 1; $i < $c; $i++)
+                    $s[$out[1][$i]] = $out[1][0];
+        return $s;
+    }
 
     public function fetch($resource)
     {
@@ -16,22 +26,24 @@ class Home
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: GET, OPTIONS');
         header("Access-Control-Allow-Headers: *");
-        
+
+        $mimeArray = self::generateUpToDateMimeArray(INPHINIT_ROOT . "mimes-types.txt");
         $resourceFile = INPHINIT_PUBLIC . $resource;
+
         if (is_file($resourceFile)) {
             $pinfo = pathinfo($resourceFile);
             if (in_array($pinfo["extension"], static::NOT_ALLOWED_EXTENSIONS)) {
-                header('X-Proj-Status: DENIED');
+                header('X-CDN-Status: NOT-ALLOWED');
                 http_response_code(403);
                 die();
             } else {
-                header('Content-Type: ' . mime_content_type($resourceFile));
+                header('Content-Type: ' . $mimeArray[$pinfo["extension"]]);
                 header('Content-Disposition: inline; filename="' . $pinfo["basename"] . '"');
-                header('X-Proj-Status: FOUND');
+                header('X-CDN-Status: HIT');
                 readfile($resourceFile);
             }
         } else {
-            header('X-Proj-Status: NOTFOUND');
+            header('X-CDN-Status: NO-HIT');
             http_response_code(404);
             die();
         }
